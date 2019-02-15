117f7c16299aa88aaf399aec8af7b9dfcb8b3747
j-shilling <shilling.jake@gmail.com>
2019-02-15 12:26:44 -0500

    Removing controller methods

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,29 +1,12 @@
 class BooksController < ApplicationController
 
-  before_action :set_store, only: [:index]
-  before_action :set_book, only: [:show, :edit, :update, :destroy]
-
   def index
-    if @store.nil?
-      @books = Book.all
-    else
-      @books = @store.books
-    end
   end
 
   def new
-    @book = Book.new
-    @book.author = Author.new
   end
 
   def create
-    @book = Book.new(book_params)
-
-    if @book.save
-      redirect_to book_path(@book)
-    else
-      render :new
-    end
   end
 
   def show
@@ -33,43 +16,9 @@ class BooksController < ApplicationController
   end
 
   def update
-    if @book.update(book_params)
-      redirect_to book_path(@book)
-    else
-      render :edit
-    end
   end
 
   def destroy
-    @book.destroy
-    if @store.nil?
-      redirect_to books_path
-    else
-      redirect_to store_books_path(@store)
-    end
-  end
-
-  private
-
-  def set_store
-    @store = Store.find(params[:store_id]) if !params[:store_id].nil?
-  end
-
-  def set_book
-    @book = Book.find(params[:id])
-  end
-
-  def book_params
-    params.require(:book).permit(
-      :title,
-      :price,
-      :author_id,
-      author_attributes: [
-        :first_name,
-        :last_name
-      ],
-      store_ids: []
-    )
   end
 
 end
--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,23 +1,12 @@
 class StoresController < ApplicationController
 
-  before_action :set_store, only: [:edit, :update, :show, :destroy]
-
   def index
-    @stores = Store.all
   end
 
   def create
-    @store = Store.new(store_params)
-
-    if @store.save
-      redirect_to store_path(@store)
-    else
-      render :new
-    end
   end
 
   def new
-    @store = Store.new
   end
 
   def show
@@ -27,25 +16,9 @@ class StoresController < ApplicationController
   end
 
   def update
-    if @store.update(store_params)
-      redirect_to store_path(@store)
-    else
-      render 'edit'
-    end
   end
 
   def destroy
-    @store.destroy
-    redirect_to stores_path
-  end
-  
-  private
-
-  def store_params
-    params.require(:store).permit(:name, :phone_number)
   end
 
-  def set_store
-    @store = Store.find(params[:id])
-  end
 end
