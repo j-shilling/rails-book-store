c1565442d2c75372ce8e04ada7b33351c48625f8
j-shilling <shilling.jake@gmail.com>
2019-02-15 12:27:28 -0500

    removing models

--- app/models/store.rb
+++
@@ -1,8 +0,0 @@
-class Store < ApplicationRecord
-  has_many :book_stores
-  has_many :books, through: :book_stores
-  has_many :authors, -> { distinct }, through: :books
-
-  validates :name, presence: true
-  validates :phone_number, format: { with: /\(?[0-9]{3}\)?-[0-9]{3}-[0-9]{4}/, message: "must be xxx-xxx-xxxx" }
-end
